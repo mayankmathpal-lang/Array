631dd096b0b809482ff75a4325ff8a83cff539c8
Mayank Mathpal <mayankmathpal939@gmail.com>
2026-01-07 03:01:33 +0530

    first commit

---
+++ LaunchAR8.java
@@ -0,0 +1,20 @@
+
+public class LaunchAR8 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//WAP to get the sum of all elements in an array
+		
+		int [] a= {20,10,30,40};
+		int sum=0;
+		
+	for (int i = 0; i < a.length; i++) {
+		sum+= a[i];
+	}
+		System.out.println("The sum is "+sum);
+		
+		
+	}
+
+}
